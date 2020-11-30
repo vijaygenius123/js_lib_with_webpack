9a2359bb6f378fc50e656772d125062f2074ab08
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-11-30 23:19:16 +0530

    Added webpack.config.js & .babelrc

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+        filename: "index_bundle.js",
+        path: path.resolve(__dirname, 'build'),
+        library: 'simpleUtils',
+        libraryTarget: "umd"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    mode: "development"
+}
